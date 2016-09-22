4992b304cdcdc0ec78e0be90cd9ed8d22767cfcd
cerbin <bleble@d.com>
2016-09-22 16:58:44 +0200

    Implemented some methods of Display class.

--- src/Display.java
+++ src/Display.java
@@ -1,9 +1,27 @@
 public class Display {
     public void displayHelloMessage(String name) {
-        System.out.println("Witaj " + name);
+        System.out.println("Hello " + name);
     }
 
     void displayInstructions() {
+        System.out.println("DisplayInstructions"); //TODO display Instructions
+    }
 
+    void displayMenu() {
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.print("Your choice: ");
+    }
+
+    void displayQuestion() {
+        //TODO
+    }
+    void displayScore() {
+
+    }
+    void displayEndMessage(String name) {
+        System.out.println("Bye " + name);
     }
 }
