f09f0fbfd6b524d3c0ebe19c9d7596105a9df774
cerbin <bleble@d.com>
2016-09-22 17:10:07 +0200

    Created class Calculator

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,11 @@
 public class Player {
+    private String name;
+    private int score = 0;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
 }
